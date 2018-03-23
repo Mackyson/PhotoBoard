a62c9b42d2231ec135d0568c93ff8d6f007ee233
Mackyson <mackyson72@gmail.com>
2018-03-23 19:45:57 +0900

    add submit-reflecting method
    ものすごーく雑な手動受付の骨組みができた。

--- screen/screen.js
+++ screen/screen.js
@@ -3,14 +3,49 @@ enchant();
 window.onload = function () {
     var width = window.parent.screen.width;
     var height = window.parent.screen.height;
-    //var spliteList = new ArrayList();
 
-    var core = new Core(width,height);
+    var core = new Core(width, height);
+    core.fps = 15;
     core.preload('bg.jpg');
     core.onload = function () {
-        var bear = new Sprite(width,height);
-        bear.image = core.assets['bg.jpg'];
-        core.rootScene.addChild(bear);
-    };
-    core.start();
+
+        var spriteList = new Array();
+        var cnt = 0;
+        var isButtonDown = false;//denied keep pushing
+
+        var bg = new Sprite(width, height);
+        bg.image = core.assets['bg.jpg'];
+
+
+        core.rootScene.addChild(bg);
+        core.rootScene.addChild(new Label("Hello!:" + cnt + "\n spriteNumber" + spriteList.length));
+
+        core.addEventListener("enterframe", function () {
+
+ 
+
+
+            if (core.input.left && !isButtonDown) {
+                cnt--;
+                isButtonDown = true;
+            }
+            if (core.input.right && !isButtonDown) {
+                cnt++;
+                isButtonDown = true;
+            }
+            if (core.input.up && !isButtonDown) {
+                core.load("../submit/upload/" + cnt + ".jpg", function () {
+                    var tmp = new Sprite(500, 500);
+                    tmp.image = core.assets["../submit/upload/" + cnt + ".jpg"];
+                    spriteList.push(tmp);
+                    core.rootScene.addChild(spriteList[cnt++]);
+                });
+
+                isButtonDown = true;
+            }
+            if (!core.input.left && !core.input.right && !core.input.up && isButtonDown) { isButtonDown = false; }
+
+    });
+};
+core.start();
 };
